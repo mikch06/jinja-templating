a3f394559d9787345340acd17994c2f71543c85c
mki <mki@scaling.ch>
2023-01-12 16:29:08 +0100

    integrate templating

--- write_message.py
+++ write_message.py
@@ -5,10 +5,16 @@ import school
 
 max_score = 100
 test_name = "Python Challenge"
+students = [
+    {"name": "Sandrine",  "score": 100, "date": "2021.10.10"},
+    {"name": "Gergeley", "score": 87, "date": "2022.12.10"},
+    {"name": "Frieda", "score": 92, "date": "2022.06.07"},
+    {"name": "Elena", "score": 92, "date": "2023.01.07"},
 
+]
 
 environment = Environment(loader=FileSystemLoader("templates/"))
-template = environment.get_template("message.html")
+template = environment.get_template("results.html")
 
 for student in school.students:
     filename = f"message_{student['name'].lower()}.txt"
@@ -20,3 +26,14 @@ for student in school.students:
     with open(filename, mode="w", encoding="utf-8") as message:
         message.write(content)
         print(f"... wrote {filename}")
+
+results_filename = "students_results.html"
+results_template = environment.get_template("results.html")
+context = {
+    "students": students,
+    "test_name": test_name,
+    "max_score": max_score,
+}
+with open(results_filename, mode="w", encoding="utf-8") as results:
+    results.write(results_template.render(context))
+    print(f"... wrote {results_filename}")
